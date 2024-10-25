556ce5ab77b702b305000a2fbb2c64da98473490
somto <somtochukwu.opara@pau.edu.ng>
2024-10-25 12:42:38 +0100

    (my second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+	println!(); // prints just a
+
+	println!("hello ");//prints
+
+	println!("format {} arguments","some"); //prints format some arguments
+
+	println!("My bame is {}. I am class rep of stream 1", "Somto Opara");
+}
