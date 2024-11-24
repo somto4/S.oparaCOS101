3424670827635a91038968eb60b966d0621cf0e7
somto4 <somtochukwu.opara@pau.edu.ng>
2024-11-24 23:58:45 +0100

    Add files via upload

---
+++ practice_4/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let fullname = "Chidubem John Umeh";
+    let department = "Computer Science";
+    let uni = "Pan Atlantic University";
+
+    let mut school = "School of science".to_string();
+    // push string
+    school.push_str(" and Technology");
+
+    println!("My name is: {}", fullname);
+    // check length
+    println!("The length of my fullname is: {}", fullname.len());
+    println!("I am a student of {} Department", department);
+    println!("{}", school);
+    println!("{}", uni);
+}
+
